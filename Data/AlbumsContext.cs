b56c495e51ad8a9e773356bb14bc2aaec6de7599
Åsa Lodesjö <54840222+asa-lo@users.noreply.github.com>
2021-02-23 10:40:53 +0100

    Add files via upload

---
+++ Data/AlbumsContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Data
+{
+    public class AlbumsContext : DbContext
+    {
+        public AlbumsContext(DbContextOptions<AlbumsContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Albums> Albums { get; set; }
+    }
+}
---
+++ Models/Albums.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Albums
+    {
+        //Properties
+        public int Id { get; set; }
+
+        public string Artist { get; set; }
+
+        public string Title { get; set; }
+
+        public int Length { get; set; }
+
+        public string Category { get; set; }
+        
+
+
+    }
+}
